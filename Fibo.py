f6af56f630eb7de93c80d2bf52832b4bfdc101d2
Fredrik-hr <1119fredrik@gmail.com>
2024-10-01 11:04:38 +0200

    Rabbit
    Test

---
+++ Fibo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 09:50:27 2024
+
+@author: 1119f
+"""
+
+def rabbit():
+    a, b = 0, 1
+    while b <= 50:
+        print(b)
+        a, b = b, a+b
+    return b
+
+rabbit()
